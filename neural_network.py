2531e2f3d87840577095e913d6a01e4ff53ab808
Daniel Goldenberg <42619318+DanielGoldenberg0@users.noreply.github.com>
2020-11-26 17:33:47 -0500

    Added saving and loading

--- neural_network.py
+++ neural_network.py
@@ -3,9 +3,10 @@ import sys
 import numpy as np
 from termcolor import colored
 
+
 class NeuralNetwork:
     def __init__(self):
-        self.weights = 3 * np.random.random((3, 1)) - 1
+        self.weights = 2 * np.random.random((3, 1)) - 1
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
     def sigmoid_derivative(self, x):
@@ -20,24 +21,33 @@ class NeuralNetwork:
         inputs = inputs.astype(float)
         output = self.sigmoid(np.dot(inputs, self.weights))
         return output
+    def save(self):
+        np.save("model.npy", self.weights)
+    def load(self):
+        self.weights = np.load("model.npy")
 
 
-if __name__ == "__main__":
-    model = NeuralNetwork()
-
+def train(model: NeuralNetwork):
     training_inputs = np.array([
-        [0, 0, 1],
-        [1, 1, 1],
-        [1, 0, 1],
-        [0, 1, 1],
-    ])
+    [0, 0, 1],
+    [1, 1, 0],
+    [1, 1, 1],
+    [0, 1, 0]])
 
     training_outputs = np.array([[0, 1, 1, 0]]).T
 
-    model.train(training_inputs, training_outputs, 100000)
+    model.train(training_inputs, training_outputs, 10000)
+    model.save()
 
-    A = int(sys.argv[1])
-    B = int(sys.argv[2])
-    C = int(sys.argv[3])
 
-    print("Prediction: ", colored(model.predict(np.array([A, B, C])), "cyan"))
+model = NeuralNetwork()
+train(model)
+
+X = int(sys.argv[1])
+Y = int(sys.argv[2])
+Z = int(sys.argv[3])
+
+prediction = model.predict(np.array([X, Y, Z]))
+
+print("Output: ", colored(prediction[0], "cyan"))
+print("Prediction: ", colored(int(round(prediction[0])), "cyan"))
